8f0828dfce90c1e1b110b94906abaabb94314a7a
jihwi <yeoukim1070@gmail.com>
2024-05-26 15:23:43 +0900

    20240526_1장_스프링프레임워크의 방식 빈 팩토리의 특징_싱글톤스코프

--- src/main/java/com/example/toby_springframework_240526/TobySpringframework240526Application.java
+++ src/main/java/com/example/toby_springframework_240526/TobySpringframework240526Application.java
@@ -13,27 +13,36 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class TobySpringframework240526Application {
+//
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        SpringApplication.run(TobySpringframework240526Application.class, args);
+//
+//
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        UserDao dao = context.getBean("userDao", UserDao.class);
+//
+//        User user = new User();
+//        user.setId("whiteship");
+//        user.setName("백기선");
+//        user.setPassword("married");
+//
+//        dao.add(user);
+//        System.out.println(user.getId() + " 등록 성공");
+//
+//        User user2 = dao.get(user.getId());
+//        System.out.println(user2.getName());
+//        System.out.println(user2.getPassword());
+//
+//        System.out.println(user2.getId() + " 조회 성공");
+//    }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        SpringApplication.run(TobySpringframework240526Application.class, args);
-
 
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        UserDao userDao1 = context.getBean("userDao", UserDao.class);
+        UserDao userDao2 = context.getBean("userDao", UserDao.class);
 
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
-
-        dao.add(user);
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
+        System.out.println(userDao1);
+        System.out.println(userDao2); //동일 객체 (싱글톤)
     }
-
 }
