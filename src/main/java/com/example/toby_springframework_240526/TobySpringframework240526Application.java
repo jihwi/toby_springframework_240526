c8604b73ee03609a7c438f8af774373b696ab9e2
jihwi <yeoukim1070@gmail.com>
2024-05-26 14:31:34 +0900

    20240526_1장_dao테스트코드

---
+++ src/main/java/com/example/toby_springframework_240526/TobySpringframework240526Application.java
@@ -0,0 +1,33 @@
+package com.example.toby_springframework_240526;
+
+import com.example.toby_springframework_240526.dao.UserDao;
+import com.example.toby_springframework_240526.domain.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class TobySpringframework240526Application {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        SpringApplication.run(TobySpringframework240526Application.class, args);
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+
+}
---
+++ src/test/java/com/example/toby_springframework_240526/TobySpringframework240526ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toby_springframework_240526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringframework240526ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
