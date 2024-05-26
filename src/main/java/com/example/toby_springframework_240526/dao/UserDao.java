0c4c2d48247bf940e96f5011cd8b55dfaec4553c
jihwi <yeoukim1070@gmail.com>
2024-05-26 17:50:41 +0900

    20240526_3장_템플릿_try-catch-finally 리소스 반납 예외처리

--- src/main/java/com/example/toby_springframework_240526/dao/UserDao.java
+++ src/main/java/com/example/toby_springframework_240526/dao/UserDao.java
@@ -41,7 +41,7 @@ public class UserDao {
         ps.setString(1, id);
         ResultSet rs = ps.executeQuery();
 
-        User user= null;
+        User user = null;
         if (rs.next()) {
             user = new User();
             user.setId(rs.getString("id"));
@@ -61,13 +61,32 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "delete from users"
-        );
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement(
+                    "delete from users"
+            );
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
     }
 
     public int getCount() throws SQLException {
