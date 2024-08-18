a9752a0709d41999d96af857227013b821bb4da0
jihwi <yeoukim1070@gmail.com>
2024-08-18 15:00:06 +0900

    240721_5장_서비스추상화_jdbc의 트랜잭션 경계설정 메소드 TrnsactionSynchronizationManager를 통해 connection 파라미터 문제 해결

--- src/main/java/com/example/toby_springframework_240526/service/UserService.java
+++ src/main/java/com/example/toby_springframework_240526/service/UserService.java
@@ -3,7 +3,12 @@ package com.example.toby_springframework_240526.service;
 import com.example.toby_springframework_240526.dao.UserDao;
 import com.example.toby_springframework_240526.domain.Level;
 import com.example.toby_springframework_240526.domain.User;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
@@ -11,18 +16,34 @@ public class UserService {
     public final static int MIN_RECCOMEND_FOR_GOLD = 30;
 
     private final UserDao userDao;
+    private final DataSource dataSource;
 
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, DataSource dataSource) {
         this.userDao = userDao;
+        this.dataSource = dataSource;
     }
 
-    public void upgradeLevels() {
-        List<User> all = userDao.getAll();
+    public void upgradeLevels() throws Exception {
+        TransactionSynchronizationManager.initSynchronization();
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        c.setAutoCommit(false);
 
-        for (User user : all) {
-            if(canUpgradeLevel(user)) {
-                upgradeLevel(user);
+        try {
+            List<User> all = userDao.getAll();
+
+            for (User user : all) {
+                if(canUpgradeLevel(user)) {
+                    upgradeLevel(user);
+                }
             }
+            c.commit();
+        }catch (Exception e) {
+            c.rollback();
+            throw e;
+        }finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
         }
     }
 
