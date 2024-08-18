36cda3e0c317d84062c6552f675553da49cf4eef
jihwi <yeoukim1070@gmail.com>
2024-08-18 16:44:29 +0900

    240721_5장_서비스추상화_메일서비스 추상화 (테스트용 mock object) 테스트 대역 중 테스트 대상으로부터 전닯다은 정보를 검증할수 잇도록 설계된것을 목 오브젝트라고 한다.

--- src/test/java/com/example/toby_springframework_240526/UserServiceTest.java
+++ src/test/java/com/example/toby_springframework_240526/UserServiceTest.java
@@ -4,12 +4,14 @@ import com.example.toby_springframework_240526.dao.DaoFactory;
 import com.example.toby_springframework_240526.dao.UserDao;
 import com.example.toby_springframework_240526.domain.Level;
 import com.example.toby_springframework_240526.domain.User;
+import com.example.toby_springframework_240526.service.MockMailSender;
 import com.example.toby_springframework_240526.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -49,15 +51,19 @@ public class UserServiceTest {
                 new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGINCOUNT_FOR_SILVER - 1, 0, new Date(), null),
                 new User("joytouch", "강명성", "p2", Level.BASIC, MIN_LOGINCOUNT_FOR_SILVER, 0, new Date(), "test@test.com"),
                 new User("erwins", "신승한", "p3", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD - 1, new Date(), null),
-                new User("madnite1", "이상호", "p4", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD, new Date(), "test@test.com"),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD, new Date(), "test2@test.com"),
                 new User("green", "오민규", "p5", Level.GOLD, 100, 100, new Date(), null)
         );
     }
 
     @Test
+    @DirtiesContext
     public void upgradeLevels() throws Exception {
         dao.deleteAll();
         for (User user : users) dao.add(user);
+
+        MockMailSender mockMailSender = new MockMailSender();
+        userService.setMailSender(mockMailSender);
 
         userService.upgradeLevels();
 
@@ -66,6 +72,11 @@ public class UserServiceTest {
         checkLevel(users.get(2), Level.SILVER);
         checkLevel(users.get(3), Level.GOLD);
         checkLevel(users.get(4), Level.GOLD);
+
+        List<String> requests = mockMailSender.getRequests();
+        assertThat(requests.size(), is(2));
+        assertThat(requests.get(0), is(users.get(1).getEmail()));
+        assertThat(requests.get(1), is(users.get(3).getEmail()));
     }
 
     private void checkLevel(User user, Level level) {
