851f877a437c198a1382b5db641cbaa0554c0ad7
jihwi <yeoukim1070@gmail.com>
2025-03-09 16:28:38 +0900

    241027_6장_aop_스프링의 프록시용 팩토리 빈 advisor = pointcut(메소드선정알고리즘) + advise (부가기능)

--- src/test/java/com/example/toby_springframework_240526/DynamicProxyTest.java
+++ src/test/java/com/example/toby_springframework_240526/DynamicProxyTest.java
@@ -1,11 +1,14 @@
 package com.example.toby_springframework_240526;
 
 import com.example.toby_springframework_240526.service.aop.*;
+import org.aopalliance.aop.Advice;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -89,5 +92,22 @@ public class DynamicProxyTest {
             String ret = (String) invocation.proceed(); // 리플렉션 method와 달리 메소드 실행시 타겟 오브젝트를 전달할 필요없다. methodInvocation은 메소드 정보와 함꼐 타깃 오브젝트를 알고 있기 때문
             return ret.toUpperCase();
         }
+    }
+
+    @Test
+    public void pointCutAdvisor() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("sayH*");
+
+        DefaultPointcutAdvisor defaultPointcutAdvisor = new DefaultPointcutAdvisor(pointcut, new UpperCaseAdvice());
+        pfBean.addAdvisor(defaultPointcutAdvisor);
+
+        Hello hello = (Hello) pfBean.getObject();
+        assertThat(hello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(hello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
     }
 }
